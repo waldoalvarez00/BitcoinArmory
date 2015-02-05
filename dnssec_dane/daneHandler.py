007d8f71d2dfbb0ef34bde258edb4083695a39df
Doug <droark@bitcoinarmory.com>
2015-02-05 18:09:58 -0500

    ARMORY-278 - Implement DANE script template handling
    Checkpoint. Minor changes made to DANE handling code. Code is basically
    complete but needs tests (to be written) and more cleanup.

--- dnssec_dane/daneHandler.py
+++ dnssec_dane/daneHandler.py
@@ -14,7 +14,6 @@ from armoryengine.ArmoryUtils import binary_to_hex # WRONG?
 from armoryengine.ConstructedScript import PublicKeySource, ConstructedScript, \
    ScriptRelationshipProof, PublicKeyRelationshipProof, BTCAID_PR_VERSION, \
    BTCAID_PAYLOAD_TYPE
-from CppBlockUtils import HDWalletCrypto
 
 PKS1NoChksum_Comp_v0 = hex_to_binary(
    "00000221 03cbcaa9 c98c877a 26977d00 825c956a 238e8ddd fbd322cc e4f74b0b"
@@ -23,11 +22,6 @@ CS1Chksum_Uncomp_v0 = hex_to_binary(
    "00000206 76a9ff01 88ac0145 00000441 04cbcaa9 c98c877a 26977d00 825c956a"
    "238e8ddd fbd322cc e4f74b0b 5bd6ace4 a77bd330 5d363c26 f82c1e41 c667e4b3"
    "561c06c6 0a2104d2 b548e6dd 059056aa 51142038 ce")
-
-# Insert later.
-#   finalPubKey = HDWalletCrypto().getChildKeyFromOps_SWIG(startPubKey,
-#                                                          multProofObj.rawMultList)
-
 
 # Function that takes a wallet ID payment request and validates it. This is the
 # entry function that launches all the steps required to regenerate all the
@@ -64,8 +58,8 @@ def validatePaymentRequest(inPayReq):
          # Have the record type recreate the script. If we receive a PKS, assume
          # a P2PKH record must be created. If we receive a CS, generate whatever
          # resides in the CS.
-         # NOTE: The return record type can vary, all types must have the same
-         # function and function prototype!
+         # NOTE: If functions share the same name across classes, it's ideal for
+         # the prototypes to match too!
          finalKey = None
          finalScript = None
          if recType == BTCAID_PAYLOAD_TYPE.InvalidRec:
@@ -73,7 +67,7 @@ def validatePaymentRequest(inPayReq):
          else:
             if recType == BTCAID_PAYLOAD_TYPE.PublicKeySource:
                # Get key and then generate a P2PKH TxOut script from it.
-               finalKey = daneReq.generateScript(inPayReq.srpLists[ctr])
+               finalKey = daneReq.generateKeyData(inPayReq.srpLists[ctr].pkrpList)
             elif recType == BTCAID_PAYLOAD_TYPE.ConstructedScript:
                finalScript = daneReq.generateScript(inPayReq.srpLists[ctr])
 
@@ -100,10 +94,10 @@ def getDANERecord(daneRecName, desiredRecType):
    retRec = None
 
    # THIS CODE WILL BE REPLACED WITH PROPER DNS CODE EVENTUALLY!!!
-   if desiredRecType eq BTCAID_PAYLOAD_TYPE.PublicKeySource:
+   if desiredRecType == BTCAID_PAYLOAD_TYPE.PublicKeySource:
       retRec = PKS1NoChksum_Comp_v0
       retType = BTCAID_PAYLOAD_TYPE.PublicKeySource
-   elif desiredRecType eq BTCAID_PAYLOAD_TYPE.ConstructedScript:
+   elif desiredRecType == BTCAID_PAYLOAD_TYPE.ConstructedScript:
       retRec = CS1NoChksum_Comp_v0
       retType = BTCAID_PAYLOAD_TYPE.ConstructedScript
    else:
